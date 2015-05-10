16fe95b0b8df84d5db01f800b98ac2854e3f3eca
Jesse Zhang <uncle.chao@live.cn>
2015-05-10 22:42:12 +0800

    css

---
+++ DIVCSSDemo/JS/headingBar.js
@@ -0,0 +1,10 @@
+﻿/// <reference path="jquery-1.8.2.js" />
+
+$(function () {
+    $(".tn-title").mouseover(function () {
+        $(this).css("background-color", "rgb(237,238,240)");
+    });
+    $(".tn-title").mouseout(function () {
+        $(this).css("background-color", "#FCFCFC");
+    });
+});
